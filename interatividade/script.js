f2dd54fdfd2d9629141711e8a8928f91eabed577
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-09-12 10:30:12 -0300

    interatividade com JS

--- interatividade/script.js
+++ interatividade/script.js
@@ -1,1 +1,23 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#fef4b7'
+        imagem.style.boxShadow = '3px 3px 10px #fef4b7'
+    }
+    else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#b85301'
+        imagem.style.boxShadow = '3px 3px 10px #b85301'
+    }
+    else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#446480'
+        imagem.style.boxShadow = '3px 3px 10px #446480'
+    }
+}
